4aaab10f70199268ba54f3dbdd3fc1bbfac7e01b
mathewpalma-collab <mathew.palma@cathcollege.school.nz>
2026-06-10 11:57:14 +1200

    new
    new

--- adventure_guy.gd
+++ adventure_guy.gd
@@ -4,17 +4,15 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 func _physics_process(delta: float) -> void:
-	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	process_movement()
 	move_and_slide()
 	
 	
 	
-	func process_movement()-> void:
+func process_movement()-> void:
 		# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_vector("left ", "right","up" , "down")
+
+
+	velocity = direction * SPEED
